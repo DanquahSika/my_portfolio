5168900b32f98d26874ce20a307a5480e1012180
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-03-26 19:03:17 +0000

    responsive nav

--- frontend/src/components/nav/Navbar.jsx
+++ frontend/src/components/nav/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/bechlogo.png";
-import hamburgerIcon from "../../assets/images/hamburger.svg";
+import hamburgerIcon from "../../assets/images/hamburgercolored.svg";
 const Navbar = () => {
   let Links = [
     { name: "About", link: "#about" },
