440442280c26a7e8eb808dcff246dcdfe6332c82
Sika Danquah <sikadanquah@Sikas-MacBook-Air.local>
2024-03-26 15:39:11 +0000

    adding colours

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colours:{
+        'peach': '#edc7b7',
+        'cream': '#eee2dc',
+        'gray': '#bab2b5',
+       ' blue': '#123c69',
+        'pink': '#ac3b61',
+    },
     extend: {},
   },
   plugins: [],
