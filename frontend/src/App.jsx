ca9ea53f6725a61dd8813325211f8cf7a4e292f0
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-03-26 16:54:12 +0000

    routing setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
-
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/Home";
+import Navbar from "./components/nav/Navbar";
+import About from "./components/about/About";
+import Blog from "./pages/blog/Blog";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
-
   return (
-    <>
-    
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
