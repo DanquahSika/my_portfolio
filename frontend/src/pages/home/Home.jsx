6643514334968901ff37ac6caeeecf1861191260
Sika Danquah <sikadanquah@Sikas-MacBook-Air.local>
2024-03-27 16:35:56 +0000

    additional changes

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,60 +1,50 @@
 import React from "react";
 
 import { Link } from 'react-router-dom';
-// import AppBanner from '../components/shared/AppBanner';
 import ProjectsGrid from '../../components/projects/ProjectsGrid';
 import { ProjectsProvider } from '../../components/projects/ProjectsContext';
 import Button from '../../components/reusable/Button';
 
 import Hero from "../../components/hero/Hero";
 import Counter from "../../components/counter/Counter";
-// import About from "../../components/about/About";
-// import ServicesTab from "../../components/services-section/ServicesTab";
-// import Contact from "../../components/contact/Contact";
-// import PortfolioTab from "../../components/portfolio-section/PortfolioTab";
-// import BlogTab from "../../components/blog-section/BlogTab";
-// import Footer from "../../components/footer/Footer";
+import About from "../../components/about/About";
+import ServicesTab from "../../components/services-section/ServicesTab";
 
-// const Home = () => {
-//   return (
-//     <div>
-      
-//       <About />
-//       <ServicesTab />
-//       <Contact />
-//       <PortfolioTab />
-//       <BlogTab />
-//       <Footer />
-//     </div>
-//   );
-// };
+import PortfolioTab from "../../components/portfolio-section/PortfolioTab";
+import BlogTab from "../../components/blog-section/BlogTab";
+import Footer from "../../components/footer/Footer";
 
-// export default Home;
 
 
 
 
 const Home = () => {
-	return (
-		<div className="container mx-auto">
-			<Hero />
+  return (
+    <div className="container mx-auto">
+      <Hero />
+     
+
+      <ProjectsProvider>
+        <ProjectsGrid></ProjectsGrid>
+      </ProjectsProvider>
+
+      <div className="mt-8 sm:mt-10 flex justify-center">
+        <Link
+          to="/projects"
+          className="font-general-medium flex items-center px-6 py-3 rounded-lg shadow-lg hover:shadow-xl bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 text-white text-lg sm:text-xl duration-300"
+          aria-label="More Projects"
+        >
+          <Button title="More Projects" />
+        </Link>
+      </div>
+      <About />
+      <ServicesTab />
       <Counter />
-
-			<ProjectsProvider>
-				<ProjectsGrid></ProjectsGrid>
-			</ProjectsProvider>
-
-			<div className="mt-8 sm:mt-10 flex justify-center">
-				<Link
-					to="/projects"
-					className="font-general-medium flex items-center px-6 py-3 rounded-lg shadow-lg hover:shadow-xl bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 text-white text-lg sm:text-xl duration-300"
-					aria-label="More Projects"
-				>
-					<Button title="More Projects" />
-				</Link>
-			</div>
-		</div>
-	);
+      <PortfolioTab />
+      <BlogTab />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
--- frontend/src/pages/projects/Projects.jsx
+++ frontend/src/pages/projects/Projects.jsx
@@ -4,7 +4,7 @@ import { ProjectsProvider } from '../../components/projects/ProjectsContext';
 const Projects = () => {
 	return (
 		<ProjectsProvider>
-			<div className="container mx-auto">
+			<div className="container mx-auto bg-blue">
 				<ProjectsGrid />
 			</div>
 		</ProjectsProvider>
