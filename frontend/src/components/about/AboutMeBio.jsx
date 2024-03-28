0fbafc0f9e0157f98e0f7130340c0cf4fa77ea1e
Sika Danquah <sikadanquah@Sikas-MacBook-Air.local>
2024-03-28 13:03:11 +0000

    profile about

--- frontend/src/components/about/AboutMeBio.jsx
+++ frontend/src/components/about/AboutMeBio.jsx
@@ -1,4 +1,4 @@
-import profileImage from '../../assets/images/profile.jpg';
+import profileImage from "../../assets/images/profile.jpg"
 import { useContext } from 'react';
 import AboutMeContext from './AboutMeContext';
 
@@ -14,7 +14,7 @@ const AboutMeBio = () => {
 			<div className="font-general-regular w-full sm:w-3/4 text-left">
 				{aboutMe.map((bio) => (
 					<p
-						className="mb-4 text-ternary-dark dark:text-ternary-light text-lg"
+						className="mb-4 text-ternary-light text-lg"
 						key={bio.id}
 					>
 						{bio.bio}
