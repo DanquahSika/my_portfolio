cc215c572644d6f706c918a09847daabd1b4ebd5
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-03-28 10:08:51 +0000

    services

--- frontend/src/components/services-section/ServicesTab.jsx
+++ frontend/src/components/services-section/ServicesTab.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import AnimateIn from "./Animation";
+import { useState, useEffect } from "react";
 
 const ServicesTab = () => {
+  const baseUrl = import.meta.env.VITE_API_URL;
+  const [skills, setSkills] = useState([]);
+  const fetchSkills = async () => {
+    const response = await fetch(`${baseUrl}/skills`);
+    const data = await response.json();
+    setSkills(data);
+  };
+  useEffect(() => {
+    fetchSkills();
+  }, []);
   return (
-    <div>ServicesTab</div>
-  )
-}
+    <>
+      <AnimateIn
+        from="opacity-0 scale-90"
+        to="opacity-100 scale-100"
+        duration={500}
+      >
+        <div>
+          <h3 className="text-center text-3xl pb-2">My Skills</h3>
+          <div className="services flex justify-center items-center gap-2">
+            {skills.map((item) => (
+              <div className="serv1  text-black bg-white p-5 text-center rounded">
+                <p>{item.skill}</p>
+                <div className="rounded-full w-24 h-24 border-2 border-black mx-auto">
+                  <span className="absolute top-1/2">100%</span>
+                </div>
+                <p>{item.proficiency}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </AnimateIn>
+    </>
+  );
+};
 
-export default ServicesTab
+export default ServicesTab;
